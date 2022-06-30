618ef71d61ba6a8ac763be5d6ee5595d79a9031d
jeaaanius <limborock15@gmail.com>
2022-06-30 21:12:35 +0800

    added js for clicking the button if it is active or not

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -128,5 +128,20 @@
                 teamsMeta.controller.control = teamsCarousel;
             }
         });
+        const formInpts = document.querySelectorAll(
+            ".floating-contact-form .form-container .form-input"
+        );
+
+        const contactIcon = document.querySelector(
+            ".floating-contact-form .contact-icon"
+        );
+
+        const formContainer = document.querySelector(
+            ".floating-contact-form .form-container"
+        );
+
+        contactIcon.addEventListener("click", () => {
+            formContainer.classList.toggle("active");
+        });
     }
 )(jQuery);
