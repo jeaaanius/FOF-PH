718ca44da752f33a85ca895cd2424f429c37deb4
Marc San Diego <74586578+marcsandiego@users.noreply.github.com>
2022-06-30 18:21:32 +0800

    added js file for project

---
+++ assets/js/main-project.js
@@ -0,0 +1,44 @@
+(function($) {
+
+	"use strict";
+
+	var fullHeight = function() {
+
+		$('.js-fullheight').css('height', $(window).height());
+		$(window).resize(function(){
+			$('.js-fullheight').css('height', $(window).height());
+		});
+
+	};
+	fullHeight();
+
+	var carousel = function() {
+		$('.featured-carousel').owlCarousel({
+	    loop:true,
+	    autoplay: true,
+		stagePadding: 100,
+	    margin: 40,
+	    animateOut: 'fadeOut',
+	    animateIn: 'fadeIn',
+	    nav:true,
+	    dots: true,
+	    autoplayHoverPause: false,
+	    items: 1,
+	    navText : ["<span class='ion-ios-arrow-back'></span>","<span class='ion-ios-arrow-forward'></span>"],
+	    responsive:{
+	      0:{
+	        items:1
+	      },
+	      600:{
+	        items:1
+	      },
+	      1000:{
+	        items:1
+	      }
+	    }
+		});
+
+	};
+	carousel();
+
+})(jQuery);
